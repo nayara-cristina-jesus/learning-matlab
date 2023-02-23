221e8780b7c810ecab005f3b10c7ff160d2db411
Nayara Cristina Jesus <122224446+nayara-cristina-jesus@users.noreply.github.com>
2023-02-22 23:09:53 -0300

    Update hello_world.m

--- hello_world.m
+++ hello_world.m
@@ -1,8 +1,11 @@
 %{
-Função 'hello_world'
-A função hello_world.m retorna a string 'Hello World!'
-A diretiva %#codegen indica que o código MATLAB destina-se à geração de código.
+Autor: Nayara Cristina Jesus
+Date: 22-02-2023
 %}
+
+% Função 'hello_world'
+% A função hello_world.m retorna a string 'Hello World!'
+% A diretiva %#codegen indica que o código MATLAB destina-se à geração de código.
   
 function y = hello_world 
 %#codegen
